a4d805c79f3afbd9bccb0b421aba8087e9c626d3
Gescof <gescolarfernandez@gmail.com>
2022-07-28 12:14:33 +0200

    Inital commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+packages = []
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+
+setup(
+    name="example_app",
+    version="0.1.0",
+    description="example api to be deployed to aws lambda",
+    url="",
+    author="Gescof",
+    author_email="",
+    license="",
+    include_package_data=True,
+    install_requires=requirements,
+    packages=packages,
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+    ],
+)
